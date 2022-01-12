a2817158ddd06a3ae191fcb5eda99fb6e6363d46
Faris Azizy <farispyurangi@gmail.com>
2022-01-12 20:56:25 +0700

    change

--- src/entities.js
+++ src/entities.js
@@ -1,10 +1,3 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-this-before-super */
-/* eslint-disable no-undef */
-/* eslint-disable no-plusplus */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-unused-vars */
-
 let ammunition = 100;
 const Storage = require('./modules/storage');
 
@@ -42,70 +35,6 @@ export class Entity extends Phaser.GameObjects.Sprite {
       }, this);
       this.setData('isDead', true);
     }
-  }
-}
-
-export class Player extends Entity {
-  constructor(scene, x, y, key) {
-    super(scene, x, y, key, 'Player');
-    this.setData('speed', 200);
-    this.setData('isShooting', false);
-    this.setData('timerShootDelay', 10);
-    this.setData('timerShootTick', this.getData('timerShootDelay') - 1);
-  }
-
-  moveUp() {
-    this.body.velocity.y = -this.getData('speed');
-  }
-
-  moveDown() {
-    this.body.velocity.y = this.getData('speed');
-  }
-
-  moveLeft() {
-    this.body.velocity.x = -this.getData('speed');
-  }
-
-  moveRight() {
-    this.body.velocity.x = this.getData('speed');
-  }
-
-  onDestroy() {
-    this.scene.time.addEvent({ // go to game over scene
-      delay: 1000,
-      callback() {
-        this.scene.scene.start('SceneScores');
-      },
-      callbackScope: this,
-      loop: false,
-    });
-  }
-
-  update() {
-    this.body.setVelocity(0, 0);
-    this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
-    this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
-
-    if (this.getData('isShooting')) {
-      if (this.getData('timerShootTick') < this.getData('timerShootDelay')) {
-        this.setData('timerShootTick', this.getData('timerShootTick') + 1); // every game update, increase timerShootTick by one until we reach the value of timerShootDelay
-      } else { // when the "manual timer" is triggered:
-        const laser = new PlayerLaser(this.scene, this.x, this.y);
-        this.scene.playerLasers.add(laser);
-
-        this.scene.sfx.laser.play(); // play the laser sound effect
-        this.setData('timerShootTick', 0);
-        ammunition--;
-        Storage.setAmmo(ammunition);
-      }
-    }
-  }
-}
-
-export class PlayerLaser extends Entity {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'sprLaserPlayer');
-    this.body.velocity.y = -200;
   }
 }
 
